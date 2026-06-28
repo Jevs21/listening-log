629fc2579cc3af84b41a42236701e950c21ecd21
Jordan Evans <jordanevans5588@gmail.com>
2026-06-28 12:17:50 -0400

    fix: port mapping was incorrect

--- server/config/config.go
+++ server/config/config.go
@@ -14,13 +14,9 @@ func Load() Config {
 	c := Config{
 		ClientID:             os.Getenv("CLIENT_ID"),
 		ClientSecret:         os.Getenv("CLIENT_SECRET"),
-		Port:                 os.Getenv("PORT"),
+		Port:                 "8080",
 		DatabaseURL:          os.Getenv("DATABASE_URL"),
 		SpotifyAllowedUserID: os.Getenv("SPOTIFY_ALLOWED_USER_ID"),
-	}
-
-	if c.Port == "" {
-		c.Port = "8080"
 	}
 	if c.DatabaseURL == "" {
 		c.DatabaseURL = "postgres://listening_log:listening_log@localhost:5432/listening_log?sslmode=disable"
